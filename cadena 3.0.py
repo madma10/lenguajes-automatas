8b1b20ff8530cc2b1dd64a9e1f13715b45ba03ee
madma10 <madrid100689@gmail.com>
2016-07-15 00:50:38 -0500

    Add files via upload
    versión mejorada de proyecto cadena en pyhton

---
+++ cadena 3.0.py
@@ -0,0 +1,27 @@
+#Yosimar Davila Garcia
+#Fernanda Aidee Garcia Novas
+#Ricardo Hernadez Carrillo
+#Socorro Abigail Cristóbal Hernández
+#Adrian Madrid Morales
+abc = []
+num = int(input("ingresa el numero de los elementos del alfabeto: "))
+for i in range(num):
+    abc.append(input("intruduce los elementos de alfabeto: "))
+print ("tu alfabeto es: ")
+print (abc)
+
+ele =int(input("ingresa el numero que deseas multiplicar el abecedario (A^x): "))
+print(ele)
+if ele== 0:
+    print ("alfabeto =[E]");
+elif ele==1:
+    print ("el nuevo alfabeto es:")
+    print (abc)
+elif ele==2:
+    
+        
+               
+               
+    
+    
+
